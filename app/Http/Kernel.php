26a8c73ab5a1953daf9b9497043f52ba600220d0
Don Gilbert <don.gilbert@mautic.com>
2018-01-19 10:32:20 -0500

    Don't throttle by default. At least not yet.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:200,1',
+            //'throttle:200,1',
             'bindings',
         ],
     ];
@@ -58,6 +58,6 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        //'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
