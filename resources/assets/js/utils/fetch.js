ef1ce4ad0bafcf224dab185c1cbae701b44a7f74
Don Gilbert <don.gilbert@mautic.com>
2018-01-23 09:53:39 -0500

    Update error handling from axios

--- resources/assets/js/utils/fetch.js
+++ resources/assets/js/utils/fetch.js
@@ -3,25 +3,38 @@ import axios from 'axios';
 
 let FileDownload = require('react-file-download');
 
+axios.interceptors.response.use(
+    (response) => {
+        return response;
+    },
+    (error) => {
+        if (error.response.status === 401) {
+            //@TODO Needs to take into account user roles
+            window.location.href = API_HOST + '/login';
+        }
+
+        return Promise.reject(error.response);
+    }
+);
+
 export default function (url, options = {}) {
-  let forAuth = (options.hasOwnProperty('forAuth') && options.forAuth);
+    let forAuth = (options.hasOwnProperty('forAuth') && options.forAuth);
 
-  let link = (url.includes(API_PATH) || forAuth)
-      ? API_HOST + url
-      : API_HOST + API_PATH + url;
-  let method = options.hasOwnProperty('method') ? options.method : 'GET';
-  let data = options.hasOwnProperty('body') ? options.body : {};
+    let link = (url.includes(API_PATH) || forAuth)
+        ? API_HOST + url
+        : API_HOST + API_PATH + url;
+    let method = options.hasOwnProperty('method') ? options.method : 'GET';
+    let data = options.hasOwnProperty('body') ? options.body : {};
 
-  return axios[method.toLowerCase()](link, data)
-      .then((response) => {
-          if (response.headers['x-suggested-filename']) {
-              return FileDownload(response.data, response.headers['x-suggested-filename']);
-          }
+    return axios[method.toLowerCase()](link, data)
+        .then((response) => {
+            console.log(response);
 
-          return response;
-      })
-      .catch((error) => {
-          // We likely have an expired JWT, so redirect to login
-          console.log(error);
-      });
+            if (response.headers['x-suggested-filename']) {
+                return FileDownload(response.data, response.headers['x-suggested-filename']);
+            }
+
+            return response;
+        })
+    ;
 }
