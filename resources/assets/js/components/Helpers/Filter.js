189e50148101ddbc18ee5e8838aecef01c83a24a
Don Gilbert <don.gilbert@mautic.com>
2018-02-02 17:21:18 -0500

    Refine search

--- resources/assets/js/components/Helpers/Filter.js
+++ resources/assets/js/components/Helpers/Filter.js
@@ -14,12 +14,25 @@ class Filter extends Component {
     _handleSubmit(event) {
         if (event.target.value.length > 3) {
             let filterFields = this.props.filterFields;
+            let searchState = null;
+
+            switch (this.props.type) {
+                case 'contacts':
+                    searchState = this.props.contactSearch;
+                    break;
+                case 'accounts':
+                    searchState = this.props.accountSearch;
+                    break;
+                case 'opportunities':
+                    searchState = this.props.opportunitySearch;
+                    break;
+            }
 
             Object.keys(filterFields).map((key) => {
-                filterFields[key] = event.target.value;
+                searchState[key] = event.target.value;
             });
 
-            this.props.dispatch(this.props.onInputChange(1, filterFields));
+            this.props.dispatch(this.props.onInputChange(1, searchState));
         }
 
         if (event.target.value.length === 0) {
