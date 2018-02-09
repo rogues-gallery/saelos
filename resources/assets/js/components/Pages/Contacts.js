feda51cbdfc7f8901a231a3e2aa3efb2fdbb25c5
Don Gilbert <don.gilbert@mautic.com>
2018-02-09 17:29:56 -0500

    Convert Contacts page to a Stateless Functional Component

--- resources/assets/js/components/Pages/Contacts.js
+++ resources/assets/js/components/Pages/Contacts.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React  from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -8,50 +8,40 @@ import { actionCreators } from '../../actions';
 import * as types from "../../actions/types";
 import ContactsList from '../Lists/ContactsList';
 
-class Contacts extends Component {
-    constructor(props) {
-        super(props);
+const toggleNewPanel = (dispatch) => {
+    document.getElementById('contact-panel-wrapper').classList.toggle('contact-panel-open');
+    document.querySelector('body').classList.toggle('panel-open');
 
-        this._toggleNewPanel = this._toggleNewPanel.bind(this);
-    }
-
-    _toggleNewPanel() {
-        document.getElementById('contact-panel-wrapper').classList.toggle('contact-panel-open');
-        document.querySelector('body').classList.toggle('panel-open');
-
-        // Set the form state for a new contact
-        this.props.dispatch({
-            type: types.FETCHING_CONTACT_FOR_FLYOUT_SUCCESS,
-            data: {
-                custom_fields: []
-            }
-        });
-    }
-
-    render() {
-        let filterFields = {
-            first_name: null,
-            last_name: null,
-            email: null
+    // Set the form state for a new contact
+    dispatch({
+        type: types.FETCHING_CONTACT_FOR_FLYOUT_SUCCESS,
+        data: {
+            custom_fields: []
         }
+    });
+};
 
-        return (
-            <Backend>
-                <div className="content-inner">
-                    <div className="content-top flex-row-even">
-                        <Filter onInputChange={actionCreators.fetchContacts} filterFields={filterFields} type="contacts" />
-                        <div className="content-top-buttons">
-                            <span className="create-button button button-primary" onClick={this._toggleNewPanel}>
-                                <span>Create Contact</span>
-                            </span>
-                        </div>
-                    </div>
-                    <ContactsList />
+const filterFields = {
+    first_name: null,
+    last_name: null,
+    email: null
+};
+
+const Contacts = ({ dispatch }) => (
+    <Backend>
+        <div className="content-inner">
+            <div className="content-top flex-row-even">
+                <Filter onInputChange={actionCreators.fetchContacts} filterFields={filterFields} type="contacts" />
+                <div className="content-top-buttons">
+                    <span className="create-button button button-primary" onClick={() => toggleNewPanel(dispatch)}>
+                        <span>Create Contact</span>
+                    </span>
                 </div>
-            </Backend>
-        );
-    }
-}
+            </div>
+            <ContactsList />
+        </div>
+    </Backend>
+);
 
 Contacts.propTypes = {
     dispatch: PropTypes.func.isRequired
