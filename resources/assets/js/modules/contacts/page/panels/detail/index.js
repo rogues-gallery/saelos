fadfde2922f4036993b80497dbe907e8a215bb81
DB Hurley <david.hurley@mautic.com>
2018-03-09 01:49:04 -0500

    Added Sample SRI data

--- resources/assets/js/modules/contacts/page/panels/detail/index.js
+++ resources/assets/js/modules/contacts/page/panels/detail/index.js
@@ -5,6 +5,7 @@ import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import Opportunities from '../../../../opportunities/partials/_opportunities'
+import SRI from '../../../../sri/partials/_sri'
 import Contact from '../../../Contact'
 import Notes from '../../../../notes/partials/_notes'
 import { getContact } from '../../../store/selectors'
@@ -57,7 +58,7 @@ const Details = ({contact, dispatch, toggle}) => (
 
           <div id="collapseSRI" className="collapse show" aria-labelledby="headingSRI">
             <div className="card-body border-bottom">
-              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              <SRI />
             </div>
           </div>
         </div>
---
+++ resources/assets/js/modules/sri/partials/_sri.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import ChartistGraph from 'react-chartist';
+import { connect } from 'react-redux';
+
+import * as MDIcons from 'react-icons/lib/md'
+
+class SRI extends React.Component {
+	render() {
+		
+		const data = {series: [[2, 3, 5, 7, 5, 8, 4, 5]] }
+		const options = {
+						low: 0,
+						fullWidth: true,
+						showArea: true, 
+						showLabel: false,
+					  axisX: {
+					    showGrid: true,
+					    showLabel: false,
+					    offset: 0
+					  },
+					  axisY: {
+					    showGrid: false,
+					    showLabel: false,
+					    offset: 0
+					  },
+}
+
+		return (
+			<div className="sriValue">
+				<div className="h1 text-center">0.75</div>
+				<div className="text-center mini-text text-muted text-uppercase pb-2"><MDIcons.MdAccessTime /> EST <span className="text-dark">3 days</span> to close</div>
+		
+				<ScoreChart data={data} options={options} type="Line" />
+
+				<div className="mini-text text-muted font-weight-bold text-uppercase mt-2">Next Action</div>
+				<p>Send an email in 3 days</p>
+			</div>
+		)
+	}
+}
+
+
+const ScoreChart = ({data, options, type}) => {
+	return (
+		<ChartistGraph data={data} options={options} type={type} className="sri-chart" />
+	)
+}
+
+export default SRI
