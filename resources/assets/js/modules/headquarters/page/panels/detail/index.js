084378d7471289f33abafd04530070edaa117bfa
DB Hurley <david.hurley@mautic.com>
2018-03-16 11:06:03 -0400

    Added Pipeline panel in headquarters

--- resources/assets/js/modules/headquarters/page/panels/detail/index.js
+++ resources/assets/js/modules/headquarters/page/panels/detail/index.js
@@ -3,10 +3,7 @@ import PropTypes from 'prop-types';
 
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { fetchContact, saveContact } from '../../../../contacts/service';
-import {getContact, getCustomFieldsForContacts, isStateDirty, getFirstContactId} from '../../../../contacts/store/selectors';
 import Headquarters from '../../../Headquarters'
-import Notes from '../../../../notes/partials/_notes'
 
 import Volume from '../partials/_volume'
 import Email from '../partials/_email'
@@ -15,9 +12,11 @@ import Team from '../partials/_team'
 import Opportunities from '../partials/_opportunities'
 import Responses from '../partials/_responses'
 
+import ContactList from '../../../../contacts/page/panels/list/components/list'
 
 import * as MDIcons from 'react-icons/lib/md'
-import ChartistGraph from 'react-chartist';
+import ChartistGraph from 'react-chartist'
+import Chartist from 'chartist'
 
 class Detail extends React.Component {
   constructor(props) {
@@ -30,11 +29,11 @@ class Detail extends React.Component {
   }
 
   componentWillMount() {
-    this.props.dispatch(fetchContact(this.props.contact.id))
+
   }
 
   componentWillReceiveProps(next) {
-    this.setState({formState: next.contact.originalProps})
+
   }
 
 
@@ -43,16 +42,19 @@ class Detail extends React.Component {
   }
 
   render() {
+
+    const contacts = {}
+
     switch(this.state.view) {
       case 'default':
-        return <Details contact={this.props.contact} dispatch={this.props.dispatch} toggle={this._toggleView} user={this.props.user} />
-      case 'contact_notes':
-        return <ContactNotes contact={this.props.contact} notes={this.props.contact.notes} dispatch={this.props.dispatch}  toggle={this._toggleView} user={this.props.user} />
+        return <Pipeline contacts={contacts} dispatch={this.props.dispatch}  toggle={this._toggleView} user={this.props.user} />
+      case 'vector':
+        return <Vector cdispatch={this.props.dispatch} toggle={this._toggleView} user={this.props.user} />
     }
   }
 }
 
-const Details = ({contact, dispatch, toggle, user}) => {
+const Vector = ({dispatch, toggle, user}) => {
 
   const data = {labels: ['V', 'E', 'C', 'T', 'O', 'R'], series: [[4, 5, 6, 7, 5, 8], [10,10,10,10,10,10]] }
   const options = {
@@ -76,13 +78,13 @@ const Details = ({contact, dispatch, toggle, user}) => {
   return (
     <div key={1} className="col detail-panel border-left">
     <div className="border-bottom py-2 heading">
-      <a href="javascript:void(0)" className="mt-1 btn btn-xs btn-outline-secondary position-fixed r-0 mr-2" onClick={() => toggle('contact_notes')}><span className="h5"><MDIcons.MdKeyboardArrowRight /></span></a>
+      <a href="javascript:void(0)" className="mt-1 btn btn-xs btn-outline-secondary position-fixed r-0 mr-2" onClick={() => toggle('default')}><span className="h5"><MDIcons.MdKeyboardArrowRight /></span></a>
         <div className="pt-1 mt-1 h5 text-center">VECTOR</div>
     </div>
     <div className="h-scroll">
         <div className="card ct-container">
           <div className="card-header" id="headingVector">
-            <h6 className="mb-0" data-toggle="collapse" data-target="#headingVector" aria-expanded="true" aria-controls="collapseSRI">
+            <h6 className="mb-0" data-toggle="collapse" data-target="#headingVector" aria-expanded="true" aria-controls="collapseVector">
               <MDIcons.MdKeyboardArrowDown /> Summary
             </h6>
           </div>
@@ -117,24 +119,53 @@ const VectorChart = ({data, options, type}) => {
   )
 }
 
-const ContactNotes = ({contact, notes, dispatch, toggle, user}) => (
-  <div key={1} className="col detail-panel border-left">
-    <div className="border-bottom py-2 heading">
-      <a href="javascript:void(0)" className="mt-1 btn btn-xs btn-outline-secondary position-fixed ml-2" onClick={() => toggle('default')}><span className="h5"><MDIcons.MdKeyboardArrowLeft /></span></a>
-        <div className="pt-1 mt-1 h5 text-center">Notes</div>
+const Pipeline = ({contacts, dispatch, toggle, user}) => {
+
+  const data = {series: [{name: 'stage', data: [10, 8, 4, 3, 1, 5] }]}
+  const options = {
+          low: 0,
+          fullWidth: true,
+          showArea: true, 
+          showLabel: false,
+          series: {
+            stage: {
+              lineSmooth: Chartist.Interpolation.step()
+            }
+          },
+          axisX: {
+            showGrid: false,
+            showLabel: false,
+            offset: 0
+          },
+          axisY: {
+            showGrid: true,
+            showLabel: false,
+            offset: 0
+          }
+        }
+  return (
+    <div key={1} className="col detail-panel border-left">
+      <div className="border-bottom py-2 heading">
+        <a href="javascript:void(0)" className="mt-1 btn btn-xs btn-outline-secondary position-fixed ml-2" onClick={() => toggle('vector')}><span className="h5"><MDIcons.MdKeyboardArrowLeft /></span></a>
+          <div className="pt-1 mt-1 h5 text-center">Pipeline</div>
+      </div>
+      <div className="h-scroll">
+         <div className="collapse show">
+              <div className="card-body border-bottom">
+                <div className="pipelineGraph">
+                  <ChartistGraph data={data} options={options} type="Line" className="graph" />
+                </div>
+              </div>
+            </div>
+      </div>
     </div>
-    <div className="h-scroll">
-      <Notes notes={notes} dispatch={dispatch} entityType="App\Person" entityId={contact.id} user={user} />
-    </div>
-  </div>
-)
+  )
+}
 
 Detail.propTypes = {
-  contact: PropTypes.object.isRequired,
   user: PropTypes.object.isRequired
 }
 
 export default withRouter(connect((state, ownProps) => ({
-  contact: getContact(state, ownProps.match.params.id || getFirstContactId(state)),
-  user: state.user,
+  user: state.user
 }))(Detail))
