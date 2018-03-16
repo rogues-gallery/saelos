7e4a8a3280537d33f3af066ad2bfdade25a0120b
Don Gilbert <don.gilbert@mautic.com>
2018-03-16 15:59:12 -0400

    Fix some formatting issues

--- resources/assets/js/modules/companies/partials/_companies.js
+++ resources/assets/js/modules/companies/partials/_companies.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types'
 import { fetchCompany } from "../../companies/service"
 import * as MDIcons from 'react-icons/lib/md'
 
-
 class Companies extends React.Component {
   render() {
     const { companies, dispatch } = this.props;
     return (
-  <div className="card">
-    <div className="card-header" id="headingCompanies">
-      <h6 className="mb-0" data-toggle="collapse" data-target="#collapseCompanies" aria-expanded="true" aria-controls="collapseCompanies">
-        <MDIcons.MdKeyboardArrowDown /> Companies <span className="text-muted font-weight-normal">({companies.length})</span>
-      </h6>
-    </div>
+      <div className="card">
+        <div className="card-header" id="headingCompanies">
+          <h6 className="mb-0" data-toggle="collapse" data-target="#collapseCompanies" aria-expanded="true" aria-controls="collapseCompanies">
+            <MDIcons.MdKeyboardArrowDown /> Companies <span className="text-muted font-weight-normal">({companies.length})</span>
+          </h6>
+        </div>
 
-    <div id="collapseCompanies" className="collapse show mh-200" aria-labelledby="headingCompanies">
-      <div className="list-group border-bottom">
-        {companies.map(company => <Company key={company.id} company={company} router={this.context.router} dispatch={dispatch} />)}
+        <div id="collapseCompanies" className="collapse show mh-200" aria-labelledby="headingCompanies">
+          <div className="list-group border-bottom">
+            {companies.map(company => <Company key={company.id} company={company} router={this.context.router} dispatch={dispatch} />)}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-)}
+    )
+  }
 }
 
 const Company = ({ company, dispatch, router }) => {
@@ -32,13 +32,12 @@ const Company = ({ company, dispatch, router }) => {
 
   return (
     <div onClick={() => openCompanyRecord(company.id)} className="list-group-item list-group-item-action align-items-start">
-      <p className="mini-text text-muted float-right"></p>
+      <p className="mini-text text-muted float-right" />
       <p><strong>{company.name}</strong>
-      <br />Secondary Detail</p>
+        <br />Secondary Detail</p>
     </div>
-  );
+  )
 }
-
 
 Companies.propTypes = {
   companies: PropTypes.array.isRequired
