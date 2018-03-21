0636a25d445e850945bd98bba0faccdfd28dba28
Don Gilbert <don.gilbert@mautic.com>
2018-03-21 15:18:54 -0400

    Redirect to first contact on load

--- resources/assets/js/modules/contacts/routes.js
+++ resources/assets/js/modules/contacts/routes.js
@@ -1,17 +1,29 @@
-import Page from './page';
+import React from 'react'
+import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
+import Page from './page'
+import {getFirstContactId} from "./store/selectors"
+import Main from '../../Main'
+
+let MyRedirect = ({firstContactId}) =>
+  <Main><Redirect to={`/contacts/${firstContactId}`} /></Main>
+
+const RedirectToFirstContact = connect(state => ({
+  firstContactId: getFirstContactId(state)
+}))(MyRedirect)
 
 export default [
   {
     path: '/',
     exact: true,
     auth: true,
-    component: Page
+    component: RedirectToFirstContact
   },
   {
     path: '/contacts',
     exact: true,
     auth: true,
-    component: Page
+    component: RedirectToFirstContact
   },
   {
     path: '/contacts/:id',
