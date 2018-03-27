8b1c6c368ba598b48fa090d08da4e0dd98ff09d7
Don Gilbert <don.gilbert@mautic.com>
2018-03-27 10:24:04 -0400

    Update to fields

--- resources/assets/js/utils/helpers/customFieldsHelper.js
+++ resources/assets/js/utils/helpers/customFieldsHelper.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import Select from 'react-select';
-import 'react-day-picker/lib/style.css';
-import _ from 'lodash';
+import React from 'react'
+import _ from 'lodash'
 
 export const getCustomField = (customField, fields) => {
-  let fieldIndex = _.findIndex(fields, (f) => f.custom_field_alias === customField);
+  let fieldIndex = _.findIndex(fields, (f) => f.custom_field_alias === customField)
 
-  return fieldIndex >= 0 ? fields[fieldIndex] : {};
+  return fieldIndex >= 0 ? fields[fieldIndex] : {}
 };
 
 export const getCustomFieldValue = (customField, fields, defaultValue) => {
-  const field = getCustomField(customField, fields);
+  const field = getCustomField(customField, fields)
 
-  return field.value ? field.value : defaultValue;
+  return field.value === null ? defaultValue : field.value
 };
--- resources/assets/js/utils/helpers/fields/FieldLayout.js
+++ resources/assets/js/utils/helpers/fields/FieldLayout.js
@@ -4,9 +4,125 @@ import _ from 'lodash'
 import Select from 'react-select'
 import * as MDIcons from 'react-icons/lib/md'
 
+import 'react-day-picker/lib/style.css'
+
 class FieldLayout extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this._buildHtml = this._buildHtml.bind(this)
+  }
+
+  _buildHtml() {
+    const { model, field, inEdit, onChange } = this.props
+
+    let fieldValue = _.get(model, field.alias);
+
+    if (typeof fieldValue === 'object') {
+      fieldValue = _.get(fieldValue, 'name')
+    }
+
+    const readOnly = !inEdit ? {
+      readOnly: true,
+      className: 'form-control-plaintext',
+      required: field.required ? 'required' : false
+    } : {
+      readOnly: false,
+      className: 'form-control',
+      required: field.required ? 'required' : false
+    }
+
+    if (inEdit) {
+      switch (field.type) {
+        case 'textarea':
+          return <textarea
+            {...readOnly}
+            id={field.alias}
+            name={field.alias}
+            onChange={onChange}
+            defaultValue={fieldValue}
+          />
+        case 'radio':
+        case 'checkbox':
+          return (
+            <label className="switch float-left mr-2">
+              <input
+                type="checkbox"
+                name={field.alias}
+                id={field.alias}
+                onChange={onChange}
+                defaultChecked={fieldValue}
+              />
+              <span className="toggle-slider round" />
+            </label>
+          )
+        case 'lookup':
+        case 'number':
+          return <input
+            type="number"
+            {...readOnly}
+            id={field.alias}
+            name={field.alias}
+            onChange={onChange}
+            defaultValue={fieldValue}
+          />
+        case 'date':
+        case 'email':
+          return <input
+            type="email"
+            {...readOnly}
+            id={field.alias}
+            name={field.alias}
+            onChange={onChange}
+            defaultValue={fieldValue}
+          />
+        case 'multiselect':
+        case 'select':
+          return <Select
+            options={Object.keys(field.options).map(v => ({value: v, label: v}))}
+            valueKey="value"
+            labelKey="label"
+            value={fieldValue}
+            className="form-control"
+            onChange={(value) => {
+              const e = {
+                target: {
+                  name: field.alias,
+                  value: value.value
+                }
+              }
+
+              onChange(e)
+            }}
+          />
+        default:
+          return <input
+            type="text"
+            {...readOnly}
+            id={field.alias}
+            name={field.alias}
+            onChange={onChange}
+            defaultValue={fieldValue}
+          />
+      }
+    } else {
+      switch (field.type) {
+        case 'url':
+          return (
+            <a className="hidden-link" href={fieldValue} target="_blank">
+              {fieldValue} <span className="text-muted"><MDIcons.MdExitToApp /></span>
+            </a>
+          )
+        case 'checkbox':
+          return fieldValue ? 'Yes' : 'No'
+        default:
+          return <div dangerouslySetInnerHTML={{__html: fieldValue}} />
+      }
+    }
+  }
+
   render() {
-    const { model, field, inEdit, onChange, ...props } = this.props;
+    const { model, field, inEdit, isAdmin } = this.props
 
     let fieldValue = _.get(model, field.alias);
 
@@ -15,73 +131,29 @@ class FieldLayout extends React.Component {
     }
 
     const hidden = inEdit ? '' : typeof fieldValue === 'undefined' || field.hidden || fieldValue.length === 0 ? 'd-none' : '';
-    const readOnly = !inEdit ? {
-      readOnly: true,
-      className: 'form-control-plaintext'
-    } : {
-      readOnly: false,
-      className: 'form-control'
-    }
 
-    if (inEdit) {
-      switch (field.type) {
-        case 'picklist':
-          return (
-            <div className={`form-group mb-2 row ${hidden}`} key={`${field.alias}-${field.field_id}`}>
-              <label htmlFor={field.alias} className="col-sm-3 col-form-label">{field.label}</label>
-              <div className="col-sm-9">
-                <Select
-                  options={Object.keys(field.options).map(v => ({value: v, label: v}))}
-                  valueKey="value"
-                  labelKey="label"
-                  value={fieldValue}
-                  className="form-control"
-                  onChange={(value) => {
-                    const e = {
-                      target: {
-                        name: field.alias,
-                        value: value.value
-                      }
-                    }
+    console.group(field.alias)
+    console.log(hidden, fieldValue)
+    console.groupEnd()
 
-                    onChange(e)
-                  }}
-                />
-              </div>
-            </div>
-          )
-          break;
-        default:
-          return (
-            <div className={`form-group mb-2 row ${hidden}`} key={`${field.alias}-${field.field_id}`}>
-              <label htmlFor={field.alias} className="col-sm-3 col-form-label">{field.label}</label>
-              <div className="col-sm-9">
-                <input type="text" {...readOnly} id={field.alias} name={field.alias} onChange={onChange} defaultValue={fieldValue} />
-              </div>
-            </div>
-          )
-      }
+    if (isAdmin) {
+      return (
+        <div className={`form-group mb-2`}>
+          <label htmlFor={field.alias}>{field.label}</label>
+          <div>
+            {this._buildHtml()}
+          </div>
+        </div>
+      )
     } else {
-      switch (field.type) {
-        case 'url':
-          return (
-            <div className={`form-group mb-2 row ${hidden}`} key={`${field.alias}-${field.field_id}`}>
-              <label htmlFor={field.alias} className="col-sm-3 col-form-label">{field.label}</label>
-              <div className="col-sm-9">
-                <a className="hidden-link form-control-plaintext" href={fieldValue} target="_blank">{fieldValue} <span className="text-muted"><MDIcons.MdExitToApp /></span></a>
-              </div>
-            </div>
-          )
-        default:
-          return (
-            <div className={`form-group mb-2 row ${hidden}`} key={`${field.alias}-${field.field_id}`}>
-              <label htmlFor={field.alias} className="col-sm-3 col-form-label">{field.label}</label>
-              <div className="col-sm-9">
-                <input type="text" {...readOnly} id={field.alias} name={field.alias} onChange={onChange} defaultValue={fieldValue} />
-              </div>
-            </div>
-          )
-      }
+      return (
+        <div className={`form-group mb-2 row ${hidden}`}>
+          <label htmlFor={field.alias} className="col-sm-3 col-form-label">{field.label}</label>
+          <div className="col-sm-9">
+            {this._buildHtml()}
+          </div>
+        </div>
+      )
     }
   }
 }
@@ -89,7 +161,8 @@ class FieldLayout extends React.Component {
 FieldLayout.propTypes = {
   model: PropTypes.object.isRequired,
   field: PropTypes.object.isRequired,
-  inEdit: PropTypes.bool.isRequired
+  inEdit: PropTypes.bool.isRequired,
+  isAdmin: PropTypes.bool
 }
 
 export default FieldLayout
