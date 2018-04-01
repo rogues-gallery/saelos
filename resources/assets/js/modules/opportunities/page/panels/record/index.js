199b02cf9910c93766dd2790d97d978eb8ca72ec
Hurley <david.hurley@mautic.org>
2018-04-01 09:40:51 -0400

    Removed unnecessary buttons on companies and opportunities (for now)

--- resources/assets/js/modules/opportunities/page/panels/record/index.js
+++ resources/assets/js/modules/opportunities/page/panels/record/index.js
@@ -133,10 +133,7 @@ class Record extends React.Component {
     return (
       <main className="col main-panel px-3">
         <div className="toolbar border-bottom py-2 heading">
-          <button className="btn btn-primary mr-3 btn-sm list-inline-item"><span className="h5"><MDIcons.MdAllInclusive /></span></button>
-          <button className="btn btn-link mr-2 btn-sm list-inline-item" onClick={() => this._setActionView('task')}><span className="h2"><MDIcons.MdPlaylistAdd /></span></button>
-          <button className="btn btn-link mr-2 btn-sm list-inline-item"><span className="h3"><MDIcons.MdInput /></span></button>
-          <button className="btn btn-link mr-2 btn-sm list-inline-item"><span className="h2"><MDIcons.MdInsertChart /></span></button>
+          <button className="btn btn-primary mr-2 btn-sm list-inline-item" onClick={() => this._setActionView('task')}><span className="h5"><MDIcons.MdPlaylistAdd /></span></button>
           <button className="btn btn-link mr-2 btn-sm list-inline-item" onClick={this._delete}><span className="h2"><MDIcons.MdDelete /></span></button>
 
           <div className="float-right text-right pt-2">
