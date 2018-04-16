a938a77740e2f2e91e61a521bb8e48ca4dc84efc
Don Gilbert <don.gilbert@mautic.com>
2018-04-16 15:02:41 -0400

    Add build script

---
+++ build.sh
@@ -0,0 +1,43 @@
+#! /bin/sh
+
+echo 'Removing previous build dir...'
+rm -rf build
+
+echo 'Making build dir...'
+mkdir build
+
+echo 'Changing dir...'
+cd build
+
+echo 'Cloning...'
+git clone --depth 1 git@github.com:saelos/saelos .
+echo 'Cloning finished.'
+
+echo 'Composer install...'
+composer install --no-dev
+
+echo 'NPM install...'
+npm install
+
+echo 'Building app...'
+npm run prod
+
+echo 'Packaging...'
+rm build.sh
+rm package-lock.json
+rm package.json
+rm phpunit.xml
+rm -rf resources/assets/js
+rm -rf resources/assets/sass
+rm -rf node_modules
+rm -rf tests
+rm server.php
+rm webpack.mix.js
+rm .babelrc
+rm .gitignore
+rm .gitattributes
+
+echo 'Compressing and zipping...'
+tar -czf ../saelos_full_install.tar.gz ./
+
+echo 'Packaging Complete.'
