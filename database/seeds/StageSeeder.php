dcde9d85c6789176987f7908cf2af6836a89b723
Don Gilbert <don.gilbert@mautic.com>
2018-04-20 09:59:47 -0400

    Fix spacing. @$%@#$ VSCode formatting

--- database/seeds/StageSeeder.php
+++ database/seeds/StageSeeder.php
@@ -6,31 +6,31 @@ use App\Stage;
 
 class StageSeeder extends Seeder
 {
-  public function run()
-  {
-    $coreFields = [
-      [
-        'name' => 'Lost',
-        'probability' => 0,
-        'active' => 0,
-        'color' => '#607d8b'
-      ],
-      [
-        'name' => 'Open',
-        'probability' => 50,
-        'active' => 1,
-        'color' => '#2196f3'
-      ],
-      [
-        'name' => 'Won',
-        'probability' => 100,
-        'active' => 0,
-        'color' => '#4caf50'
-      ],
-    ];
+    public function run()
+    {
+        $stages = [
+            [
+                'name' => 'Lost',
+                'probability' => 0,
+                'active' => 0,
+                'color' => '#607d8b'
+            ],
+            [
+                'name' => 'Open',
+                'probability' => 50,
+                'active' => 1,
+                'color' => '#2196f3'
+            ],
+            [
+                'name' => 'Won',
+                'probability' => 100,
+                'active' => 0,
+                'color' => '#4caf50',
+            ],
+        ];
 
-    foreach ($coreFields as $field) {
-      Stage::create($field);
+        foreach ($stages as $stage) {
+            Stage::create($stage);
+        }
     }
-  }
 }
--- database/seeds/StatusSeeder.php
+++ database/seeds/StatusSeeder.php
@@ -6,31 +6,31 @@ use App\Status;
 
 class StatusSeeder extends Seeder
 {
-  public function run()
-  {
-    $coreFields = [
-      [
-        'name' => 'Cold',
-        'active' => 0,
-        'color' => '#2196f3',
-        'ordering' => 1
-      ],
-      [
-        'name' => 'Warm',
-        'active' => 1,
-        'color' => '#ff9800',
-        'ordering' => 2
-      ],
-      [
-        'name' => 'Hot',
-        'active' => 0,
-        'color' => '#f44336',
-        'ordering' => 3
-      ],
-    ];
+    public function run()
+    {
+        $statuses = [
+            [
+                'name' => 'Cold',
+                'active' => 0,
+                'color' => '#2196f3',
+                'ordering' => 1
+            ],
+            [
+                'name' => 'Warm',
+                'active' => 1,
+                'color' => '#ff9800',
+                'ordering' => 2
+            ],
+            [
+                'name' => 'Hot',
+                'active' => 0,
+                'color' => '#f44336',
+                'ordering' => 3
+            ],
+        ];
 
-    foreach ($coreFields as $field) {
-      Status::create($field);
+        foreach ($statuses as $status) {
+            Status::create($status);
+        }
     }
-  }
 }
