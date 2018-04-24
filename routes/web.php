c8d1ff8c7d996db3a8e2c7d0d01e0e0a313d5b6b
Don Gilbert <don.gilbert@mautic.com>
2018-04-24 15:35:36 -0400

    Add default authorization routes for oauth (required since we switched to a react-based login

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,28 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 Route::get('/logout', function () {
     Auth::logout();
 
     return redirect('/login');
 });
 
+Route::group(['prefix' => 'auth'], function () {
+    // Authentication Routes...
+    Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Auth\LoginController@login');
+    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+    // Password Reset Routes...
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+});
+
 Broadcast::routes();
 
-Route::get('/{any}', function() {
+Route::get('/{any}', function () {
     return view('react.index');
 })->where('any', '.*');
