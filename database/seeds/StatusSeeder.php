92c4dd349e19e1e5fe3fcf49d607f8336c6c2125
Don Gilbert <don.gilbert@mautic.com>
2018-04-27 10:34:47 -0400

    Fix DB seeder for statuses. (no active column)

--- database/seeds/StatusSeeder.php
+++ database/seeds/StatusSeeder.php
@@ -11,19 +11,16 @@ class StatusSeeder extends Seeder
         $statuses = [
             [
                 'name' => 'Cold',
-                'active' => 0,
                 'color' => '#2196f3',
                 'ordering' => 1
             ],
             [
                 'name' => 'Warm',
-                'active' => 1,
                 'color' => '#ff9800',
                 'ordering' => 2
             ],
             [
                 'name' => 'Hot',
-                'active' => 0,
                 'color' => '#f44336',
                 'ordering' => 3
             ],
