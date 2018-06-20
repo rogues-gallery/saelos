312072d546900eb25e2b6af5604b904eff419816
Don Gilbert <don.gilbert@mautic.com>
2018-06-20 18:53:28 -0400

    Add vendor files for apidoc

---
+++ resources/views/vendor/apidoc/partials/frontmatter.blade.php
@@ -0,0 +1,13 @@
+title: Saelos API Reference
+
+language_tabs:
+- bash
+- javascript
+- php
+
+includes:
+
+search: true
+
+toc_footers:
+- <a href='http://github.com/mpociot/documentarian'>Documentation Powered by Documentarian</a>
---
+++ resources/views/vendor/apidoc/partials/info.blade.php
@@ -0,0 +1,6 @@
+# Info
+
+Welcome to the Saelos API reference.
+@if($showPostmanCollectionButton)
+[Get Postman Collection]({{url($outputPath.'/collection.json')}})
+@endif
