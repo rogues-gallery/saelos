f3f762eace345ac4a3915a27d4301ba1c94792c9
Don Gilbert <don.gilbert@mautic.com>
2018-07-18 09:50:26 -0400

    Add accept header info to generated docs

--- resources/views/vendor/apidoc/partials/info.blade.php
+++ resources/views/vendor/apidoc/partials/info.blade.php
@@ -7,3 +7,5 @@ Welcome to the Saelos API reference.
 
 The examples you see to the right use [`GuzzleHttp`](https://packagist.org/packages/guzzlehttp/guzzle)
 for PHP, `jQuery` for javascript, and `cURL` for bash.
+
+When making requests to the API, be sure that you're sending along an `Accept: application/json` HTTP header.
