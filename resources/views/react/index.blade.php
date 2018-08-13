c96918c69af59be647db4ac2ee339bda21990980
Naomi C. Bush <hello@naomicbush.com>
2018-08-13 00:26:05 -0400

    A very dirty fix for the fatal error when trying to retrieve dynamic settings during the Saelos load process. Fix #61

--- resources/views/react/index.blade.php
+++ resources/views/react/index.blade.php
@@ -33,7 +33,7 @@
 
         window.SAELOS_CONFIG = {
             APP_URL: "{{ env('APP_URL') }}",
-@foreach (config('settings') as $key => $value)
+@foreach (\App\Settings::where('user_id', '')->orWhereNull('user_id')->get() as $key => $value)
             {{ strtoupper($key) }}: "{{ $value }}",
 @endforeach
             BROADCAST_DRIVER: "{{ env('BROADCAST_DRIVER') }}"
