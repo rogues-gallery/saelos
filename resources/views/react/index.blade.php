f9bb5ba1a139666f5b3ada7f3739c0443746eb16
Don Gilbert <don.gilbert@mautic.com>
2018-08-13 09:53:23 -0400

    Use default [] for config('settings') in the react index

--- resources/views/react/index.blade.php
+++ resources/views/react/index.blade.php
@@ -33,7 +33,7 @@
 
         window.SAELOS_CONFIG = {
             APP_URL: "{{ env('APP_URL') }}",
-@foreach (\App\Settings::where('user_id', '')->orWhereNull('user_id')->get() as $key => $value)
+@foreach (config("settings", []) as $key => $value)
             {{ strtoupper($key) }}: "{{ $value }}",
 @endforeach
             BROADCAST_DRIVER: "{{ env('BROADCAST_DRIVER') }}"
